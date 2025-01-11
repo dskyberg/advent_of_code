2da952a6f5270ad0322644db3d35cebd829656ed
David Skyberg <davidskyberg@gmail.com>
2025-01-11 09:29:15 -0500

    support for Matrix<u8>

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -4,6 +4,7 @@ pub mod aoc;
 pub mod convert;
 pub mod dag;
 pub mod digits;
+pub mod direction;
 pub mod matrix;
 pub mod point;
 
@@ -11,5 +12,6 @@ pub use aoc::*;
 pub use convert::*;
 pub use dag::*;
 pub use digits::*;
+pub use direction::*;
 pub use matrix::*;
 pub use point::*;
