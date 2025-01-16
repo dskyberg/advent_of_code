4e51f01c0442e77b286b2d56a5ad3d830c2f326c
David Skyberg <davidskyberg@gmail.com>
2025-01-16 07:37:15 -0500

    Day 9

---
+++ 2024/day_9/src/bin/part1.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+
+use aoc_utils::*;
+use day_9::*;
+
+fn sort_blocks(blocks: &[Option<usize>]) -> Vec<Option<usize>> {
+    let mut result = Vec::new();
+    let mut front = 0;
+    let mut back = blocks.len() - 1;
+
+    while front <= back {
+        match blocks[front] {
+            Some(block) => {
+                result.push(Some(block));
+            }
+            None => {
+                while blocks[back].is_none() {
+                    back -= 1;
+                }
+                result.push(blocks[back]);
+                back -= 1;
+            }
+        }
+        front += 1;
+    }
+
+    result
+}
+
+fn main() -> Result<()> {
+    let mut aoc = Aoc::part1();
+
+    let disk_map = read_data(INPUT);
+    let blocks = decode(&disk_map);
+    let blocks = sort_blocks(&blocks);
+    let result = calc_check_sum(&blocks);
+
+    aoc.result(result);
+    println!("{}", aoc);
+    Ok(())
+}
---
+++ 2024/day_9/src/bin/part2.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+
+use aoc_utils::*;
+use day_9::*;
+
+fn main() -> Result<()> {
+    let  result = 0;
+    let mut aoc = Aoc::part2();
+
+    // Placeholder to kill warning
+    println!("{}", INPUT);
+
+    aoc.result(result);
+    println!("{}", aoc);
+    Ok(())
+}
